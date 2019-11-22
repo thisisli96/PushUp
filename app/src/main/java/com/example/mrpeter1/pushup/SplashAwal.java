7de5d7bace18053371b059f49881711980c4296f
muh ikhsan <thisisli96@yahoo.com>
2019-11-22 23:12:08 +0700

    modified mainactivity and add menu splashawal

--- app/src/main/java/com/example/mrpeter1/pushup/SplashAwal.java
+++ app/src/main/java/com/example/mrpeter1/pushup/SplashAwal.java
@@ -4,14 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-
+public class SplashAwal extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        int i;
-        i = 2;
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash_awal);
     }
 }
